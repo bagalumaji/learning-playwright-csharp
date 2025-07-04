f1d5df1fc5d35d819f92aa485180962075b07c2c
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-04 19:34:55 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace learning_playwright_csharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+           
+        }
+    }
+}
