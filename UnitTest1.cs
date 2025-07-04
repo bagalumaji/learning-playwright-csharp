b1241070e852ea1763dcafb852a37ef9f5736824
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-04 19:36:30 +0530

    added code for sample test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,7 +13,12 @@ namespace learning_playwright_csharp
         [Test]
         public async Task Test1()
         {
-           
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Channel = "msedge" });
+            var browserContext = await browser.NewContextAsync();
+            var page = await browserContext.NewPageAsync();
+            await page.GotoAsync("https://google.com/");
+            await browser.CloseAsync();
         }
     }
 }
