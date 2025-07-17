b6e78c71604fc89ce76f50c40d2fc9e5c6fe2710
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-17 10:45:48 +0530

    added test for basic operation as click, fill,ckeck, title, url

---
+++ BasicOpearation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace learning_playwright_csharp
+{
+    public class BasicOpearation
+    {
+        [Test]
+        public async Task BasicOperationTest()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Channel = "msedge" });
+            var browserContext = await browser.NewContextAsync();
+            var page = await browserContext.NewPageAsync();
+            await page.GotoAsync("https://testautomationpractice.blogspot.com/");
+            Console.WriteLine("Title : " + await page.TitleAsync());
+            Console.WriteLine("URL : " + page.Url);
+            await page.GetByPlaceholder("Enter Name").FillAsync("SayajiRaje");
+            await page.GetByPlaceholder("Enter EMail").FillAsync("Sharu@bagal.com");
+            await page.GetByLabel("Male", new PageGetByLabelOptions { Exact = true }).CheckAsync();
+            await page.GetByLabel("Sunday").CheckAsync();
+            await page.WaitForTimeoutAsync(6000); 
+        }
+    }
+}
