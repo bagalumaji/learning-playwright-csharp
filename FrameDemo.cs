455535ba4551fc0e85a18a32b2b0d0ef793267d5
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-18 19:49:20 +0530

    frame handling

---
+++ FrameDemo.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace learning_playwright_csharp
+{
+    public class FrameDemo
+    {
+        [Test]
+        public async Task FrameDemoTest()
+        {
+            IPlaywright playwright = await Playwright.CreateAsync();
+            IBrowser browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless=false});
+            IBrowserContext browserContext = await browser.NewContextAsync();
+            IPage page = await browserContext.NewPageAsync();
+            await page.GotoAsync("https://www.lambdatest.com/selenium-playground/iframe-demo/");
+            await page.FrameLocator("#iFrame1").Locator("//div[text()='Your content.']").FillAsync("SayajiRaje");
+            await page.WaitForTimeoutAsync(6000);
+        }
+    }
+}
