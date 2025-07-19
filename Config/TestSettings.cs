c2d68cebdd9fb18ff78e786519e05a86097def0d
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:32:09 +0530

    added for test settings

---
+++ Config/TestSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace learning_playwright_csharp.Config
+{
+    public class TestSettings
+    {
+        public bool Headless { get; set; }
+        public bool DevTool { get; set; }
+        public string Channel { get; set; }
+        public int SlowMo { get; set; }
+    }
+}
