b7b29b1df16b9604578d6ed1531c32fec8f8e275
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:32:26 +0530

    updatedn test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,4 @@
+using learning_playwright_csharp.Config;
 using learning_playwright_csharp.Driver;
 using Microsoft.Playwright;
 
@@ -13,11 +14,15 @@ namespace learning_playwright_csharp
         [Test]
         public async Task Test1()
         {
-            //var playwright = await Playwright.CreateAsync();
-            //var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Channel = "msedge" });
-            //var browserContext = await browser.NewContextAsync();
-            //var page = await browserContext.NewPageAsync();
-            PlaywrightDriver playwrightDriver = new PlaywrightDriver();
+            TestSettings testSettings = new TestSettings();
+
+            testSettings.Headless = false;
+            testSettings.DevTool = true;
+            testSettings.Channel = "chrome";
+                testSettings.SlowMo = 500;
+           
+
+            PlaywrightDriver playwrightDriver = new PlaywrightDriver(testSettings);
             IPage page = await playwrightDriver.InitializePLaywright();
             await page.GotoAsync("https://google.com/");
 
