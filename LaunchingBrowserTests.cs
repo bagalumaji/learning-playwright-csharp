919515f0d3c18fcbe5cf84efbd606743023212e9
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:09:08 +0530

    added edge browser test

--- LaunchingBrowserTests.cs
+++ LaunchingBrowserTests.cs
@@ -46,6 +46,20 @@ namespace learning_playwright_csharp
             await browserContext.CloseAsync();
             await browser.CloseAsync();
         }
-        
+        [Test]
+        public async Task LaunchEdgeBrowserTest()
+        {
+            IPlaywright playwright = await Playwright.CreateAsync();
+            IBrowser browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Channel = "msedge" });
+            IBrowserContext browserContext = await browser.NewContextAsync();
+            IPage page = await browserContext.NewPageAsync();
+            await page.GotoAsync("https://www.google.com");
+            Console.WriteLine(await page.TitleAsync());
+            Console.WriteLine(page.Url);
+            await page.CloseAsync();
+            await browserContext.CloseAsync();
+            await browser.CloseAsync();
+        }
+       
     }
 }
