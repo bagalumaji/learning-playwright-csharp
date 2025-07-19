3a2aba27e76a2862c3b14059dcb49ea833011efc
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:32:18 +0530

    updated playwright driver

--- Driver/PlaywrightDriver.cs
+++ Driver/PlaywrightDriver.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.Playwright;
+﻿using learning_playwright_csharp.Config;
+using Microsoft.Playwright;
 
 namespace learning_playwright_csharp.Driver
 {
     public class PlaywrightDriver
     {
-        public async Task<IPage> InitializePLaywright()
+        public async Task<IPage> InitializePLaywright(TestSettings testSettings)
         {
             IPlaywright playwright = await Playwright.CreateAsync();
-            BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions
-            {
-                Headless = false
-            };
+            BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions();
+            launchOptions.Headless = testSettings.Headless;
+            launchOptions.SlowMo = testSettings.SlowMo;
+            launchOptions.Channel =testSettings.Channel;
             IBrowser browser = await playwright.Chromium.LaunchAsync(launchOptions);
             IBrowserContext context = await browser.NewContextAsync();
             IPage page = await context.NewPageAsync();
