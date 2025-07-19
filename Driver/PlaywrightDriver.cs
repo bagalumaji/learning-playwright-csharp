f755a99f795aab40c7f13a3a743b82f054fe9841
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:19:24 +0530

    created new class for create a playwright driver

---
+++ Driver/PlaywrightDriver.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+
+namespace learning_playwright_csharp.Driver
+{
+    public class PlaywrightDriver
+    {
+        public async Task<IPage> InitializePLaywright()
+        {
+            IPlaywright playwright = await Playwright.CreateAsync();
+            IBrowser browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false // Set to true for headless mode
+            });
+            IBrowserContext context = await browser.NewContextAsync();
+            IPage page = await context.NewPageAsync();
+            return page;
+
+        }
+    }
+}
