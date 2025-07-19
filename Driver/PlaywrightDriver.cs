4263a81fe30a298bfed0a55a90692f6644402e53
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 13:28:07 +0530

    updated

--- Driver/PlaywrightDriver.cs
+++ Driver/PlaywrightDriver.cs
@@ -1,5 +1,6 @@
 ﻿using learning_playwright_csharp.Config;
 using Microsoft.Playwright;
+using System.Threading.Channels;
 
 namespace learning_playwright_csharp.Driver
 {
@@ -7,16 +8,35 @@ namespace learning_playwright_csharp.Driver
     {
         public async Task<IPage> InitializePLaywright(TestSettings testSettings)
         {
-            IPlaywright playwright = await Playwright.CreateAsync();
-            BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions();
-            launchOptions.Headless = testSettings.Headless;
-            launchOptions.SlowMo = testSettings.SlowMo;
-            launchOptions.Channel =testSettings.Channel;
-            IBrowser browser = await playwright.Chromium.LaunchAsync(launchOptions);
+            IBrowser browser = await GetBrowserAsync(testSettings);
             IBrowserContext context = await browser.NewContextAsync();
             IPage page = await context.NewPageAsync();
             return page;
+        }
+        public async Task<IBrowser> GetBrowserAsync(TestSettings testSettings)
+        {
+            IPlaywright playwright = await Playwright.CreateAsync();
+            BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions
+            {
+                Headless = testSettings.Headless,
+                SlowMo = testSettings.SlowMo
+            };
 
+            switch (testSettings.DriverType)
+            {
+                case DriverType.Chrome:
+                    launchOptions.Channel = "chrome";
+                    return await playwright.Chromium.LaunchAsync(launchOptions);
+                case DriverType.Firefox:
+                    return await playwright.Firefox.LaunchAsync(launchOptions);
+                case DriverType.Webkit:
+                    return await playwright.Webkit.LaunchAsync(launchOptions);
+                case DriverType.Edge:
+                    launchOptions.Channel = "msedge";
+                    return await playwright.Chromium.LaunchAsync(launchOptions);
+                default:
+                    return await playwright.Chromium.LaunchAsync(launchOptions);
+            }
         }
     }
 }
