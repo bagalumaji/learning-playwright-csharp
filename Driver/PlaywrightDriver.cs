418b6c538ffe5e9d225018bf0b445ed50a2a2c22
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 12:25:43 +0530

    added code for launch option

--- Driver/PlaywrightDriver.cs
+++ Driver/PlaywrightDriver.cs
@@ -7,10 +7,11 @@ namespace learning_playwright_csharp.Driver
         public async Task<IPage> InitializePLaywright()
         {
             IPlaywright playwright = await Playwright.CreateAsync();
-            IBrowser browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions
             {
-                Headless = false // Set to true for headless mode
-            });
+                Headless = false
+            };
+            IBrowser browser = await playwright.Chromium.LaunchAsync(launchOptions);
             IBrowserContext context = await browser.NewContextAsync();
             IPage page = await context.NewPageAsync();
             return page;
