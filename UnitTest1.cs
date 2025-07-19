40f5dac51cfb277d94a80e1cee44b7090144664e
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-19 13:28:23 +0530

    test updated

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -14,14 +14,13 @@ namespace learning_playwright_csharp
         [Test]
         public async Task Test1()
         {
-            TestSettings testSettings = new TestSettings();
-
-            testSettings.Headless = false;
-            testSettings.DevTool = true;
-            testSettings.Channel = "chrome";
-            testSettings.SlowMo = 500;
+            TestSettings testSettings = new TestSettings
+            {
+                Headless = false,
+                DriverType = DriverType.Webkit,
+                SlowMo = 500,
+            };
            
-
             PlaywrightDriver playwrightDriver = new PlaywrightDriver();
             IPage page = await playwrightDriver.InitializePLaywright(testSettings);
             await page.GotoAsync("https://google.com/");
