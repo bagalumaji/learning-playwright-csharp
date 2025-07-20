d8315a6d27b0f25ef382f3bade11c231a6f7f5d9
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-20 08:57:22 +0530

    added constants

---
+++ Constants/FrameworkConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace learning_playwright_csharp.Constants
+{
+    public class FrameworkConstants
+    {
+        public const string EDGE_CHANNEL = "msedge";
+        public const string CHROME_CHANNEL = "chrome";
+    }
+}
