70dfe5b67df4cb19998bda2875dd7d8650f7d99a
Umaji Bagal <bagalumaji@gmail.com>
2025-07-21 17:08:15 +0530

    added teardown

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,25 +6,34 @@ namespace learning_playwright_csharp
 {
     public class Tests
     {
+        
         private IPage _page;
+        private PlaywrightDriver _playwrightDriver;
         [SetUp]
         public async Task Setup()
         {
             TestSettings testSettings = new TestSettings
             {
                 Headless = false,
-                DriverType = DriverType.Webkit,
+                DriverType = DriverType.Chrome,
                 SlowMo = 500,
             };
            
-            PlaywrightDriver playwrightDriver = new PlaywrightDriver();
-            _page = await playwrightDriver.InitializePLaywright(testSettings);
+             _playwrightDriver = new PlaywrightDriver();
+            _page = await _playwrightDriver.InitializePLaywright(testSettings);
         }
 
         [Test]
         public async Task Test1()
         {
             await _page.GotoAsync("https://google.com/");
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _playwrightDriver.Browser.CloseAsync();
+            await _playwrightDriver.Browser.DisposeAsync();
         }
     }
 }
