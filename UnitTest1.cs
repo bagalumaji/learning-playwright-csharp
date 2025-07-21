568d03d971d07d407b167ec18a4c9310eda525d0
Umaji Bagal <bagalumaji@gmail.com>
2025-07-22 01:24:28 +0530

    test updated with propertoes

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,13 +7,13 @@ namespace learning_playwright_csharp
         [Test]
         public async Task Test1()
         {
-            await _playwrightDriver.Page.Result.GotoAsync("https://google.com/");
+            await _playwrightDriver.Page.GotoAsync("https://google.com/");
         }
 
         [Test]
         public async Task Test2()
         {
-            await _playwrightDriver.Page.Result.GotoAsync("https://github.com/");
+            await _playwrightDriver.Page.GotoAsync("https://github.com/");
         }
     }
 }
