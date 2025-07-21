0c96b2bde237e5345f7ced30c0e5220fbe95262d
Umaji Bagal <bagalumaji@gmail.com>
2025-07-21 18:38:35 +0530

    updated test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,7 +1,4 @@
-using learning_playwright_csharp.Config;
-using learning_playwright_csharp.Driver;
 using learning_playwright_csharp.TestBase;
-using Microsoft.Playwright;
 
 namespace learning_playwright_csharp
 {
@@ -10,13 +7,13 @@ namespace learning_playwright_csharp
         [Test]
         public async Task Test1()
         {
-            await Page.GotoAsync("https://google.com/");
+            await _playwrightDriver.Page.Result.GotoAsync("https://google.com/");
         }
 
         [Test]
         public async Task Test2()
         {
-            await Page.GotoAsync("https://github.com/");
+            await _playwrightDriver.Page.Result.GotoAsync("https://github.com/");
         }
     }
 }
